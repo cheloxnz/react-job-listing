16d52389648edf09351387f2c2e78475a85ad70d
Marcelo Del Valle <cheloxnz@gmail.com>
2020-10-18 19:59:56 -0300

    Configurando

--- src/components/JobBoardComponent.js
+++ src/components/JobBoardComponent.js
@@ -30,22 +30,46 @@ const JobBoardComponent = ({ job: {
     location,
     languages,
     tools,
-} }) => (
+} }) => {
+    const tags = [role, level];
 
-        <div className='flex bg-white shadow-md m-5 p-4'>
+    if (languages) {
+        tags.push(...languages)
+    }
+    if (tools) {
+        tags.push(...tools)
+    }
+
+    return (
+        <div className='flex bg-white shadow-md m-5 p-6 rounded'>
             <div>
                 <img src={logo} alt={company} />
             </div>
-            <div className='ml-4'>
-                <h3 className='font-bold text-teal-500'>{company}</h3>
+            <div className='flex flex-col justify-between ml-4'>
+                <h3 className='font-bold text-teal-500'>
+                    {company}
+                    {isNew && (
+                        <span className='bg-teal-500 text-teal-100 font-bold m-2 py-1 px-2 rounded-full'>
+                            New
+                        </span>
+                    )}
+                    {isNew && (
+                        <span className='bg-gray-800 font-bold py-1 px-2 rounded-full'>
+                            Featured
+                        </span>
+                    )}
+                </h3>
                 <h2 className='font-bold text-lg'>{position}</h2>
                 <p className='text-gray-500'>
                     {postedAt} • {contract} • {location}
                 </p>
             </div>
-            <div></div>
+            <div className='flex items-center justify-end ml-auto'>
+                {tags ? tags.map((tag) => (<span className='text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded'>{tag}</span>)) : ''}
+            </div>
         </div>
     )
+}
 
 
 
