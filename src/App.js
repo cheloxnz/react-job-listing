881fe648d310a5940017fad10d521c1b5ffb7b2e
Marcelo Del Valle <cheloxnz@gmail.com>
2020-10-18 00:12:36 -0300

    Config data.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import data from './assets/data.json';
+import JobBoardComponent from './components/JobBoardComponent'
+
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => setJobs(data), []);
+
+  console.log(jobs)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-4xl'>Hello</h1>
+      {
+        jobs.length === 0 ? (
+          <p>Jobs are fetching...</p>
+        ) : (
+            jobs.map((job) => (
+              <JobBoardComponent
+                job={job}
+                key={job.id}
+              />
+            ))
+          )
+      }
     </div>
   );
 }
